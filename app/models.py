397c81e9716fb313ff9a07216e78905dcee226bb
rightlag <rightlag@gmail.com>
2015-02-01 21:25:41 +0000

    Added Keypair and Policy models to API MON-10

--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Keypair(models.Model):
+    access_key = models.CharField(max_length=255)
+    secret_key = models.CharField(max_length=255)
+    user = models.ManyToManyField(User)
+
+    def __unicode__(self):
+        return self.access_key
+
+class Policy(models.Model):
+    name = models.CharField(max_length=255)
+    created = models.DateTimeField()
+    policy = models.TextField()
+    user = models.ManyToManyField(User)
+
+    def __unicode__(self):
+        return self.name
